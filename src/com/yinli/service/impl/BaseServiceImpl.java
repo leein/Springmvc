d2a889bc6190a7bf068dee7a6b2dc0adfae2514e
leein <350541237@qq.com>
2017-05-25 18:33:48 +0800

    20170525利用idea2017提交的

--- src/com/yinli/service/impl/BaseServiceImpl.java
+++ src/com/yinli/service/impl/BaseServiceImpl.java
@@ -55,7 +55,7 @@ public class BaseServiceImpl<Pojo extends BaseObject, PK extends Serializable> i
 	@Override
 	public boolean exists(PK id) {
 		// TODO Auto-generated method stub
-		System.out.println("配置完github链接后,第二次将测试git文件上传!");
+		System.out.println("配置完github链接后,第三次次将测试git文件上传!");
 		return baseDao.exists(id);
 	}
 
